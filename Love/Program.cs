a1a1b1ab7b7a72b0e8c33a8341b71d178ddf53ce
Evgeniy <zheinirk38@gmail.com>
2022-08-29 23:05:09 +0800

    add HelloConsole Love project

---
+++ Love/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your name: ");
+String name = Console.ReadLine();
+String love = "zheka";
+if (name.ToLower() == love) {
+    Console.Write("Hello my love " + name + "! <3");
+}
+else {
+    Console.WriteLine("Hello, " + name + "!");
+}
